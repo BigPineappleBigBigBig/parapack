5b335f223099d681294f7a74da739b18396eb003
jianjing.xu <jianjing.xu>
2024-03-05 15:37:07 +0800

    新闻接口联调ok

--- utils/public.js
+++ utils/public.js
@@ -353,3 +353,21 @@ export const getNumberClass = (value) => {
     return 'pp-text-t1';
 };
 
+export const formatData = (timer) => {
+    if(!timer) return '';
+    const year = timer.getFullYear();
+    const month = timer.getMonth() + 1; // 由于月份从0开始，因此需加1
+    const day = timer.getDate();
+    const hour = timer.getHours();
+    const minute = timer.getMinutes();
+    const second = timer.getSeconds();
+    return `${pad(year, 4)}-${pad(month)}-${pad(day)} ${pad(hour)}:${pad(minute)}:${pad(second)}`;
+};
+// 定义具体处理标准
+// timeEl 传递过来具体的数值：年月日时分秒
+// total 字符串总长度 默认值为2
+// str 补充元素 默认值为"0"
+function pad(timeEl, total = 2, str = '0') {
+    return timeEl.toString().padStart(total, str);
+}
+
